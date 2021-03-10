0240f302bf043b4346c02cf4f9e551247e3f3680
Lucas Angelo <angelucas@gmail.com>
2021-03-09 22:34:16 -0300

    Initial project setup

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-// app.js
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-/**
- * Required External Modules
- */
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
- const express = require('express')
+var app = express();
 
-/**
- * App Variables
- */
- 
- const app = express()
- const port = 3000
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-/**
- *  App Configuration
- */
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Routes Definitions
- */
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
- app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-  })
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-/**
- * Server Activation
- */
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
- app.listen(port, () => {
-   console.log(`Example app listening at http://localhost:${port}`)
- })
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
